ae24b215c2f4cc374752401d04b32ed3ea9ec54e
sbal13 <sbal13@gmail.com>
2018-07-31 11:15:54 -0400

    done merging

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -7,14 +7,14 @@ const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
-	render() {
-		return (
-			<div className="app">
-				<SushiContainer  />
-				<Table />
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="app">
+        <SushiContainer  />
+        <Table />
+      </div>
+    );
+  }
 }
 
 export default App;
